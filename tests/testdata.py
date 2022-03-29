8e295c23e2a8db3b659783201897211a02d405eb
Marcelus Trojahn <mtrojahn@gmail.com>
2022-03-29 19:39:00 -0300

    faker and kwargs on testdata

--- tests/testdata.py
+++ tests/testdata.py
@@ -1,13 +1,21 @@
 import uuid
 
+from faker import Faker
 from sqlalchemy.orm import Session
 
 from models.symptoms import Symptom
 
+fake = Faker()
 
-def create_symptom(session: Session) -> Symptom:
-    new_id = str(uuid.uuid4())
-    symptom = Symptom(name="test_symptom", id=new_id)
+
+def create_symptom(session: Session, **kwargs) -> Symptom:
+    props = {
+        "id": str(uuid.uuid4()),
+        "name": fake.name()
+    }
+    props.update(kwargs)
+
+    symptom = Symptom(**props)
     session.add(symptom)
     session.commit()
     return symptom
